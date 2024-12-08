37e00bec7fe89d45ef764f3a77a7e35f3b16fb11
deividas strole <deiwis@yahoo.com>
2024-12-07 19:30:46 -0800

    finished with index.html by ai

--- src/main/java/com/example/TestGPTJava/TestGptJavaApplication.java
+++ src/main/java/com/example/TestGPTJava/TestGptJavaApplication.java
@@ -7,12 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestGptJavaApplication implements CommandLineRunner {
 
 	@Autowired
 	OpenAiChatModel chatModel;
+
+	@GetMapping("/welcome")
+	public String funnyJokes(@RequestParam String type) {
+		String input = String.format("Tell me a %s joke", type);
+		Prompt prompt = new Prompt(input);
+		ChatResponse response = chatModel.call(prompt);
+		return response.getResult().getOutput().getContent();
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestGptJavaApplication.class, args);
@@ -20,8 +33,5 @@ public class TestGptJavaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Prompt prompt = new Prompt("Tell me a programmer joke that is funny!");
-		ChatResponse response = chatModel.call(prompt);
-		System.out.println(response.getResult().getOutput().getContent());
 	}
 }
